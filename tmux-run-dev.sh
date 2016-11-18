68151f7e3481ec4ffc9e536ffcb7d937ba482fad
Jeb Beich <jebbeich@gmail.com>
2016-11-18 09:00:43 -0600

    Add rlwrap to debug script

--- tmux-run-dev.sh
+++ tmux-run-dev.sh
@@ -8,6 +8,6 @@ fi
 tmux new-window -n 'nvim-debug'
 tmux send-keys "NVIM_LISTEN_ADDRESS=127.0.0.1:7777 nvim" C-m
 tmux split
-tmux send-keys 'java -cp "$(cat .classpath)" clojure.main' C-m
+tmux send-keys 'rlwrap java -cp "$(cat .classpath)" clojure.main' C-m
 tmux send-keys '(println "Plugin repl connecting to nvim")' C-m
 tmux send-keys '(go)' C-m
