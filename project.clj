d56da6de85093dbbb18c0c22218808735b891aa3
Jeb Beich <jebbeich@gmail.com>
2017-03-09 23:39:22 -0600

    Bump version to 0.1.0

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject neovim-client "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject neovim-client "0.1.0"
+  :description "Client library for the Neovim RPC API"
+  :url "https://github.com/clojure-vim/neovim-client"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0-alpha12"]
